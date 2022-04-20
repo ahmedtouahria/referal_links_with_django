41097c1581cdcb1eb36b79fff718061f45240b4b
A-Amine Touahria <62561354+ahmedtouahria@users.noreply.github.com>
2022-04-20 18:08:14 +0200

    Update models.py

--- models.py
+++ models.py
@@ -5,20 +5,14 @@ from .utils import generate_random_code
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(blank=True, null=True)
-    image = models.ImageField(upload_to=upload_image_path_profile, default=None, null=True, blank=True)
+    image = models.ImageField(upload_to='profil/', default=None, null=True, blank=True)
     code = models.CharField(max_length=12,blank=True)
     recommended_by = models.ForeignKey(User,on_delete=models.CASCADE,blank=True, null=True,related_name='ref_by')
     isPartner = models.BooleanField(default=False)
     profit=models.FloatField(default=0.0)
     def __str__(self):
         return str(self.user)
-    def country(self):
-        return self.user.country
-    def num_of_partner(self):
-        partners = Profile.objects.filter(isPartner=True)
-        return len(partners)       
-        
-        
+    # override save method for create code when saved
     def save(self, *args, **kwargs):
        if self.code=='':
            code = generate_random_code()
